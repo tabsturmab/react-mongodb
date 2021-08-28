18b7edf8c94cb333d6ae38e03344e6924d15b634
Andre Silva <tadsturmab2014@gmail.com>
2021-08-28 11:23:35 -0300

    Project created

---
+++ projetomongodb/src/routes.js
@@ -0,0 +1,24 @@
+const express = require ('express');
+
+const routes = express.Router();
+
+const Usuario = require('./controllers/usuario.controller')
+const Produto = require('./controllers/produto.controller')
+
+routes.get('/', Usuario.index);
+
+//Rotas de usuários
+routes.post('/api/usuarios',Usuario.create);
+routes.get('/api/usuarios',Usuario.index);
+routes.get('/api/usuarios.details/:_id',Usuario.details);
+routes.delete('/api/usuarios/:_id',Usuario.delete);
+routes.put('/api/usuarios',Usuario.update);
+
+//Rotdas de produtos
+routes.post('/api/produtos',Produto.create);
+routes.get('/api/produtos',Produto.index);
+routes.get('/api/produtos.details/:_id',Produto.details);
+routes.delete('/api/produtos/:_id',Produto.delete);
+routes.put('/api/produtos',Produto.update);
+
+module.exports = routes;
