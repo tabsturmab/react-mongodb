e3e642ea31577ef198117b5901e2099e7d09889d
Andre Silva <tadsturmab2014@gmail.com>
2021-08-29 10:28:00 -0300

    Install material-ui

--- projetomongodb/client/src/App.js
+++ projetomongodb/client/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Inicio from './pages/client/painel'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/*<header className="App-header">
         <h1>Curso de React com MongoDB</h1>
-      </header>
+      </header>*/}
+  <Inicio/>
     </div>
   );
 }
