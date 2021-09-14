b8442564fc810fcd7638426e7bc582fb7e134616
Andre Silva <tadsturmab2014@gmail.com>
2021-09-14 08:48:26 -0300

    Login backend

--- projetomongodb/src/controllers/usuario.controller.js
+++ projetomongodb/src/controllers/usuario.controller.js
@@ -1,4 +1,6 @@
 const Usuario = require ('../models/usuario.model');
+const jwt = require ("jsonwebtoken");
+const secret = "mysecret";
 
 module.exports = {
     async index(req, res){
@@ -35,5 +37,23 @@ module.exports = {
         const user = await Usuario.findOneAndUpdate({_id},data,{new:true});
         res.json(user);
     },
+    async login(req, res){
+        const {email, senha} = req.body;
+        Usuario.findOne({email_usuario: email, tipo_usuario:1}, function(err,user){
+            if(err){
+                console.log(err);
+                res.status(200).json({erro: "Erro no servidor. Por favor, tente novamente"});
+            }else if (!user){
+                res.status(200).json({status:2, error: 'Email ou senha não conferem!'});
+            }else{
+                const payload = { email };
+                const token = jwt.sign(payload, secret, {
+                    expiresIn: '24h'
+                })
+                res.cookie('token', token, {httpOnly: true});
+                res.status(200).json({status:1, auth:true, token:token, id_client: user._id, user_name:user.nome_usuario})
+            }
+        })
+    }
 
 }
